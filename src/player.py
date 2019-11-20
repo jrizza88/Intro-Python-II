62e1c48a84b14855a9c2624812f8469022a38b54
Jamar Torres <Jetorres88@gmail.com>
2019-11-19 21:20:57 -0500

    fixed attr of Player class to match directions

--- src/player.py
+++ src/player.py
@@ -1,18 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room_player):
+    def __init__(self, name, current_room):
         self.name = name
-        self.current_room_player = current_room_player
+        self.current_room = current_room
 
     def __str__(self):
-        return f'Our adventurer {self.name} is currently in room {self.current_room_player}'
-
-    def current_room(self):
-        return f''
+        return f'Our adventurer {self.name} is currently in room {self.current_room}'
 
     def room_info(self):
-        return f'player class has: Player({self.name}), Room name:{self.current_room_player.name}. description: {self.current_room_player.description}'
+        return f'player class has: Player({self.name}), Room name:{self.current_room.name}. description: {self.current_room.description}'
 
     
 # print(Player.room_info)
